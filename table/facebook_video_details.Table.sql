b25d4acb2ebc131c6030ca0bed08733ac556ed5e
Purgo AI <support@purgo.ai>
2024-09-22 17:05:31 -0500

    Commit message

---
+++ table/facebook_video_details.Table.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE purgo_ai.facebook_video_details (
+	description VARCHAR(255), 
+	updated_time TIMESTAMP WITHOUT TIME ZONE, 
+	video_id VARCHAR(255), 
+	insert_dttm TIMESTAMP WITHOUT TIME ZONE
+)
+
