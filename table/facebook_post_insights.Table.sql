eba5024cac2195d740f3f00411fd4e76d9db7369
Purgo AI <support@purgo.ai>
2024-09-22 17:05:29 -0500

    Commit message

---
+++ table/facebook_post_insights.Table.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE purgo_ai.facebook_post_insights (
+	post_id VARCHAR(255), 
+	post_likescount VARCHAR(255), 
+	post_commentscount VARCHAR(255), 
+	permalink VARCHAR(255), 
+	status_type VARCHAR(255), 
+	createdtimestamp TIMESTAMP WITHOUT TIME ZONE, 
+	post_shares VARCHAR(255), 
+	post_message VARCHAR(255), 
+	post_hashtags VARCHAR(255), 
+	city VARCHAR(255), 
+	country VARCHAR(255), 
+	latitude VARCHAR(255), 
+	longitude VARCHAR(255), 
+	zipcode VARCHAR(255), 
+	insert_dttm TIMESTAMP WITHOUT TIME ZONE
+)
+
