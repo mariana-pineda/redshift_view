e421925a4b44cd46ef34d0f0991537b6f7192f2b
Purgo AI <support@purgo.ai>
2024-09-22 17:05:28 -0500

    Commit message

---
+++ table/facebook_post_details.Table.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE purgo_ai.facebook_post_details (
+	createdtimestamp TIMESTAMP WITHOUT TIME ZONE, 
+	story VARCHAR(255), 
+	post_id VARCHAR(255), 
+	post_message VARCHAR(255), 
+	insert_dttm TIMESTAMP WITHOUT TIME ZONE
+)
+
