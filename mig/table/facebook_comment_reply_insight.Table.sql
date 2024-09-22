3077d35b08cd8103d5ead3e45d42954dcd736c0b
Purgo AI <support@purgo.ai>
2024-09-22 17:08:40 -0500

    Commit message

---
+++ mig/table/facebook_comment_reply_insight.Table.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE purgo_ai.facebook_comment_reply_insight (
+	commentid STRING, 
+	commentmessage STRING, 
+	permalink STRING, 
+	replycount BIGINT, 
+	likecount BIGINT, 
+	reaction STRING, 
+	application_name STRING, 
+	replied_from STRING, 
+	replied_message STRING, 
+	replied_message_id STRING, 
+	insert_dttm TIMESTAMP
+)
