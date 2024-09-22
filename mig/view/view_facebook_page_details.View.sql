24afaff703172d5df70525bbaac9624343c8b6fe
Purgo AI <support@purgo.ai>
2024-09-22 17:09:00 -0500

    Commit message

---
+++ mig/view/view_facebook_page_details.View.sql
@@ -0,0 +1,7 @@
+
+CREATE OR REPLACE VIEW view_facebook_page_details AS
+SELECT facebook_page_details.page_id, 
+       facebook_page_details.page_followers, 
+       facebook_page_details.page_fans, 
+       facebook_page_details.insert_dttm
+FROM purgo_ai.facebook_page_details;
