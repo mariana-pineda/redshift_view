3ba39a62d7b97ddc98dae815d87658bb76ba29be
Purgo AI <support@purgo.ai>
2024-09-22 17:05:25 -0500

    Commit message

---
+++ table/facebook_comments_insight.Table.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE purgo_ai.facebook_comments_insight (
+	postid VARCHAR(255), 
+	comment_id VARCHAR(255), 
+	comment_message VARCHAR(255), 
+	insert_dttm TIMESTAMP WITHOUT TIME ZONE
+)
+
