ae0bf82ebc1bce4117bd55e875ae652aeba64af4
Purgo AI <support@purgo.ai>
2024-09-22 17:05:39 -0500

    Commit message

---
+++ view/view_facebook_comment_reply_insights.View.sql
@@ -0,0 +1,3 @@
+create view "view_facebook_comment_reply_insights" AS 
+ SELECT cri.commentid, cri.commentmessage, cri.permalink, cri.replycount, cri.likecount, cri.reaction, cri.application_name, cri.replied_from, cri.replied_message, cri.replied_message_id, cri.insert_dttm
+   FROM purgo_ai.facebook_comment_reply_insight cri
