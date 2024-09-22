97e7fc330651a54a34311d9b8671172203469401
Purgo AI <support@purgo.ai>
2024-09-22 17:08:49 -0500

    Commit message

---
+++ mig/table/facebook_video_insights.Table.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE purgo_ai.facebook_video_insights (
+	videoid BIGINT, 
+	total_video_10s_views BIGINT, 
+	total_video_15s_views BIGINT, 
+	total_video_avg_time_watched BIGINT, 
+	total_video_complete_views BIGINT, 
+	total_video_impressions BIGINT, 
+	total_video_view_total_time BIGINT, 
+	total_video_views BIGINT, 
+	total_video_views_autoplayed BIGINT, 
+	total_video_views_clicked_to_play BIGINT, 
+	total_video_views_unique BIGINT, 
+	insert_dttm TIMESTAMP
+)
