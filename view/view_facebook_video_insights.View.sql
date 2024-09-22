6b5b32f23dc78f8fda9d28955eca183414dd7330
Purgo AI <support@purgo.ai>
2024-09-22 17:05:36 -0500

    Commit message

---
+++ view/view_facebook_video_insights.View.sql
@@ -0,0 +1,4 @@
+create view "view_facebook_video_insights" AS 
+ SELECT vd.video_id, vd.description, vd.updated_time, vi.total_video_10s_views, vi.total_video_15s_views, vi.total_video_avg_time_watched, vi.total_video_complete_views, vi.total_video_impressions, vi.total_video_views, vi.total_video_views_autoplayed, vi.total_video_views_clicked_to_play, vi.total_video_views_unique
+   FROM purgo_ai.facebook_video_details vd
+   LEFT JOIN purgo_ai.facebook_video_insights vi ON vd.video_id::text = vi.videoid::text
