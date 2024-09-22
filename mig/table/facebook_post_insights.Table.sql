9e9d76d4c172fe495982ee8dc86d5adddd6d06cb
Purgo AI <support@purgo.ai>
2024-09-22 17:08:45 -0500

    Commit message

---
+++ mig/table/facebook_post_insights.Table.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE purgo_ai.facebook_post_insights (
+    post_id STRING, 
+    post_likescount STRING, 
+    post_commentscount STRING, 
+    permalink STRING, 
+    status_type STRING, 
+    createdtimestamp TIMESTAMP, 
+    post_shares STRING, 
+    post_message STRING, 
+    post_hashtags STRING, 
+    city STRING, 
+    country STRING, 
+    latitude STRING, 
+    longitude STRING, 
+    zipcode STRING, 
+    insert_dttm TIMESTAMP
+)
